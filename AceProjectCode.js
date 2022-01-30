ad285a48777628ca48d677b4ebab84349d21f6e5
Athen Martinez <athenmrtnz@yahoo.com>
2022-01-29 21:07:44 -0500

    Hi

--- AceProjectCode.js
+++ AceProjectCode.js
@@ -1,14 +1,1 @@
-document.write('Hello World')
-// what is this//
-<<<<<<< HEAD
-//IDK//
-//Smokin out the window//
-// yo //
-=======
-//IDK//
->>>>>>> master
-<<<<<<< HEAD
-// i am not really sure what is happeing//
-=======
-// thsi tiem is better//
->>>>>>> master
+//Hi//
